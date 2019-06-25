40099a4e228e141a5f997a0b585b561204f246b8
Prajval Raval <41849970+PrajvalRaval@users.noreply.github.com>
2019-06-25 18:50:08 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -7,18 +7,11 @@ Flash Briefing Skill - Powered By Rocket.Chat
 
 Deploy your Flash briefing skill in minutes using Rocket chat and broadcast messages on both channel and skill in realtime.
 
-# Repository Contents
-
-* `index.js` - Back-End Logic for the Alexa Flash Briefing Skill hosted on [Heroku](https://www.heroku.com/)
-* `app.json` - It is a manifest format for describing web apps. It declares environment variables, add-ons, and other information required to run an app on Heroku.
-
-
 # Setup
 
 ### Pre-requisites
 
 * Node.js (> v8.10)
-* Register for a [Heroku Account](https://www.heroku.com/)
 * Register for an [Amazon Developer Account](https://developer.amazon.com/)
 * Rocket Chat Server updated to Release 1.2.0-rc.0 or later.
 
@@ -38,23 +31,15 @@ Deploy your Flash briefing skill in minutes using Rocket chat and broadcast mess
 
 ### Deploying Code
 
-* Click on the following button,
-
-  [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
-  
 * **env Variables:**
 
-  * **CACHE_TTL** : It is used by Redis to store your data for the given period of time. You may enter your desire frequency in seconds. For ex: If you are going to post news every 24 Hrs enter `86400`. Now your data will be updated every 24 hrs. This step helps admins to save cost as by caching data.
+  * **PORT** : Enter The Port number you want server to run on.
   
   * **CHANNEL_NAME** : Enter the name of the broadcasting channel we created earlier. Make sure its in lower case with no spaces.
-  
-  * **REDIS_URL** : A Redis instance is automatically created when you deploy this app and its location is stored in this variable.
-  
-  * **SERVER_URL** : Enter your current Rocket.Chat server url here. Ex- https://your.sever.chat
-  
-  * **TITLE_TEXT** : The title of the feed item to display in the Alexa app.
 
-* After App is deployed you can find your **API endpoint URL** here, **Manage App** -> **Settings** -> **Domains and certificates** -> **Domain**. We will using be using this URL in the next step.
+  * **SERVER_URL** : Enter your current Rocket.Chat server url here. Ex- https://your.sever.chat
+
+* After App is deployed we will be using **Server URL** in the next step.
 
 ### Creating Flash Briefing Skill
 
@@ -66,7 +51,7 @@ Deploy your Flash briefing skill in minutes using Rocket chat and broadcast mess
 
 * Choose **Content type** as **Text**.
 
-* In **Feed**, paste the **API endpoint URL**.
+* In **Feed**, paste the **Server URL**.
 
 * Upload Photo and hit **Add**.
 
@@ -76,7 +61,5 @@ Deploy your Flash briefing skill in minutes using Rocket chat and broadcast mess
 
 Need community help in the following:
 
-* externalize it to a nodeJS server, to handle "hundreds of flash briefings across hundreds of RCs" for millions of users - reduce Lamda cost and create  "flash breifing publishing as a service"
-* add caching to significantly reduce lambda cost 
 * add a conversation flow to allow an admin to "publish the briefing" daily using the VUI
 * add support for audio content for more personal flash breifing delivery
--- index.js
+++ index.js
@@ -1,61 +1,74 @@
 const express = require("express");
 const axios = require('axios');
 const app = express();
+var cache = require('memory-cache');
+
+require('dotenv').config()
+app.use(express.json());
 
 // Environment Variables
 
 const PORT = process.env.PORT || 3000;
-var client = require('redis').createClient(process.env.REDIS_URL);
-
-// Environment Variables
-
 const serverurl = process.env.SERVER_URL;
 const channelName = process.env.CHANNEL_NAME;
-const flashBriefingTitle = process.env.TITLE_TEXT;
-const cacheTimeout = process.env.CACHE_TTL;
 
+//Data Frequency Timing in ms & Title Text
 
-client.on('error', (err) => {
-  console.log("Error " + err);
-});
+const cacheTimeout = 300000; //300000ms = 5mins
+const flashBriefingTitle = 'Rocket Chat Flash Briefing';
 
-app.use(express.json());
+//PING ROUTE
 
+app.get('/ping', (req,res) => {
 
-app.get('/', (req, res) => {
+  console.log('PING Request');
 
-  return client.get(`redisdb:message`, (err, result) => {
+  const pongData = ('{"data":"PONG"}');
+  var pong = JSON.parse(pongData);
+  return res.status(200).json(pong);
 
-    if (result) {
-      const resultJSON = JSON.parse(result);
-      return res.status(200).json(resultJSON);
-    } else {
-      return axios.get(`${ serverurl }/api/v1/channels.anonymousread?roomName=${ channelName }`)
-        .then(response => {
+})
 
-          const responseJSON = JSON.stringify({
-            uid: response.data.messages[0]._id,
-            updateDate: response.data.messages[0].ts,
-            titleText: flashBriefingTitle,
-            mainText: response.data.messages[0].msg,
-            redirectionUrl: `${ serverurl }/channel/${ channelName }`
-          });
+//MAIN ROUTE
 
-          client.setex(`redisdb:message`, cacheTimeout, responseJSON);
-          
-          const result = JSON.parse(responseJSON);
-          return res.status(200).json(result);
-        })
-        .catch(err => {
-          return res.json(err);
+app.get('/', (req,res) => {
+
+  if (cache.get('message')) {
+
+    console.log('Using Cached Data From Memory');
+
+    const resultJSON = JSON.parse(cache.get('message'));
+    return res.status(200).json(resultJSON);
+
+  } else {
+
+    console.log('Getting New Data From Rocket Chat Server');
+
+    return axios.get(`${ serverurl }/api/v1/channels.anonymousread?roomName=${ channelName }`)
+      .then(response => {
+
+        const responseJSON = JSON.stringify({
+          uid: response.data.messages[0]._id,
+          updateDate: response.data.messages[0].ts,
+          titleText: flashBriefingTitle,
+          mainText: response.data.messages[0].msg,
+          redirectionUrl: `${ serverurl }/channel/${ channelName }`
         });
-    }
-  });
+
+        console.log('Storing Data In Memory.')
+        cache.put('message', responseJSON, cacheTimeout);
+
+        const result = JSON.parse(responseJSON);
+        return res.status(200).json(result);
+      })
+      .catch(err => {
+        console.log(err);
+        return res.status(200).json(err);
+      });
+
+  }
 });
-
-
-
 
 app.listen(PORT, function () {
-  console.log(`Listening on Port ${PORT}`);
+  console.log(`Server Now Listening on Port ${PORT}`);
 });
